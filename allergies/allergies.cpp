b3fab85f90cc219968aa7394ba07181fdddaf563
EItan Barylko <eitan@barylko.com>
2023-08-29 20:50:49 -0700

    Included list from the std

--- allergies/allergies.cpp
+++ allergies/allergies.cpp
@@ -6,16 +6,16 @@ namespace allergies {
   bool allergy_test::is_allergic_to(std::string allergy) {
       return allergens.count(allergy);
     }
-  std::unordered_set<std::string> add_allergies(int score) {
-    if (score == 0) {
-      return allergens;
-    }
-    int tmp_score = score;
-    while (tmp_score >= 0 ) {
-      return 1;
-    }
+  // std::unordered_set<std::string> add_allergies(int score) {
+  //   if (score == 0) {
+  //     return allergens;
+  //   }
+  //   int tmp_score = score;
+  //   while (tmp_score >= 0 ) {
+  //     return 1;
+  //   }
 
-  }
+  // }
 
   };
 // }  // namespace allergies
--- allergies/allergies.h
+++ allergies/allergies.h
@@ -2,16 +2,15 @@
 #define ALLERGIES_H
 #include <string>
 #include <unordered_set>
-#include <map>
 #include <tuple>
-
+#include <list>
 
 namespace allergies {
   class allergy_test {
   private:
     int allergy_score;
     std::unordered_set<std::string> allergens;
-    // std::list<int> values{256, 128, 64, 32, 16, 8, 4, 2, 1}
+    std::list<int> values{256, 128, 64, 32, 16, 8, 4, 2, 1};
     // std::list<std::string> names{"cats" "pollen" "chocolate" "tomatoes" "strawberries" "shellfish" "peanuts" "eggs"}
     // std::list<std::tuple<std::string, int>> names_to_score(std::views::zip(names, values))
 
