65a56f9272ddfca33c05830aa148aebc52ad7690
EItan Barylko <eitan@barylko.com>
2023-09-01 22:18:34 -0700

    Changed allergy_test constructor so it creates the list of allergies

--- allergies/allergies.cpp
+++ allergies/allergies.cpp
@@ -1,21 +1,41 @@
 #include "allergies.h"
 #include <string>
+#include <map>
+#include <list>
+#include <set>
+
+using namespace std;
 
 namespace allergies {
 
-  bool allergy_test::is_allergic_to(std::string allergy) {
-      return allergens.count(allergy);
-    }
-  // std::unordered_set<std::string> add_allergies(int score) {
-  //   if (score == 0) {
-  //     return allergens;
-  //   }
-  //   int tmp_score = score;
-  //   while (tmp_score >= 0 ) {
-  //     return 1;
-  //   }
-
-  // }
-
+  const map<string, int> names_to_score {
+    { "cats", 128},
+    { "pollen", 64},
+    { "chocolate", 32 },
+    { "tomatoes", 16},
+    { "strawberries", 8},
+    { "shellfish", 4},
+    { "peanuts", 2},
+    { "eggs", 1}
   };
-// }  // namespace allergies
+
+  unordered_set<string> actual_allergies;
+
+  allergy_test::allergy_test(int score) {
+    set<pair<string, int>> allergens;
+    copy_if(names_to_score.begin(), names_to_score.end(), inserter(allergens, allergens.end()), [score](auto const & allergy) {
+      return score & allergy.second;
+    });
+
+    transform(allergens.begin(), allergens.end(), inserter(actual_allergies, actual_allergies.end()), [](auto const &pair) { return pair.first; });
+  }
+
+  bool allergy_test::is_allergic_to(std::string allergy) {
+    return actual_allergies.count(allergy);
+  }
+
+  const unordered_set<string>& get_allergies() {
+    return actual_allergies;
+  }
+
+};
--- allergies/allergies.h
+++ allergies/allergies.h
@@ -2,23 +2,12 @@
 #define ALLERGIES_H
 #include <string>
 #include <unordered_set>
-#include <tuple>
-#include <list>
 
 namespace allergies {
   class allergy_test {
   private:
-    int allergy_score;
-    std::unordered_set<std::string> allergens;
-    std::list<int> values{256, 128, 64, 32, 16, 8, 4, 2, 1};
-    // std::list<std::string> names{"cats" "pollen" "chocolate" "tomatoes" "strawberries" "shellfish" "peanuts" "eggs"}
-    // std::list<std::tuple<std::string, int>> names_to_score(std::views::zip(names, values))
-
     public:
-    allergy_test(int score) {
-      allergy_score = score;
-      // allergies = add_allergies(allergies, score);
-    }
+    allergy_test(int score); 
     std::unordered_set<std::string> add_allergies(int score);
     bool is_allergic_to(std::string allergy);
   };
